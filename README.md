018d51f2659b85cd53a9aa8da348bd9d396b0000
Niel20-web <barehnathaniel3020@gmail.com>
2026-09-08 21:51:04 +0530

    initial flutter app

---
+++ README.md
@@ -0,0 +1,3 @@
+# medivault_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/medivault_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.medivault_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
