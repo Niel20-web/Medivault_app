d1a34f3635f027b134624b9266c93c7e63ebc22f
Niel20-web <barehnathaniel3020@gmail.com>
2026-09-10 23:31:36 +0530

    build logic screen

---
+++ lib/utils/appcolors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Appcolors {
+  static const Color primary = Color(0xFF159EDD);
+ static const Color deepBlue = Color(0xFF0878C9);
+  static const Color cyan = Color(0xFF22C7E8);
+
+  static const Color background = Color(0xFFF2F8FA);
+  static const Color surface = Color(0xFFFFFFFF);
+
+  static const Color primaryText = Color(0xFF172B3A);
+  static const Color secondaryText = Color(0xFF64748B);
+
+  static const Color border = Color(0xFFDCE8ED);
+
+  static const Color success = Color(0xFF22B573);
+  static const Color warning = Color(0xFFF4A340);
+  static const Color error = Color(0xFFE05252); 
+
+}
